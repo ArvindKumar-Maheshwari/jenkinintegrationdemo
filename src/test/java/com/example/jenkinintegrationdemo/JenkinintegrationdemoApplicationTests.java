80279cdac04fef45d10074724d48052a3fe0f6e6
CTSArvind <er.arvindmaheshwari@gmail.com>
2024-07-21 20:49:37 +0530

    first commit

---
+++ src/test/java/com/example/jenkinintegrationdemo/JenkinintegrationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinintegrationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
